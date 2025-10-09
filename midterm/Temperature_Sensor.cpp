c530787f00ec16754d31d1df0806a5b57b531e0f
Imong-Jayy <andrei0436iz@gmail.com>
2025-10-09 22:37:04 +0800

    Update Temperature_Sensor.cpp

--- midterm/Temperature_Sensor.cpp
+++ midterm/Temperature_Sensor.cpp
@@ -1,15 +1,18 @@
 #include "Temperature_Sensor.h"
 
+// Constructor: Initializes the temperature sensor with the specified pin.
 TemperatureSensor::TemperatureSensor(byte pin) {
   _pin = pin;
   _oneWire = new OneWire(_pin);
   _sensors = new DallasTemperature(_oneWire);
 }
 
+// Initializes the DallasTemperature sensors.
 void TemperatureSensor::begin() {
   _sensors->begin();
 }
 
+// Requests temperature and returns it in Fahrenheit.
 float TemperatureSensor::getFahrenheit() {
   _sensors->requestTemperatures();
   float tempC = _sensors->getTempCByIndex(0);
@@ -17,6 +20,7 @@ float TemperatureSensor::getFahrenheit() {
   return tempF;
 }
 
+// Requests temperature and returns it in Celsius.
 float TemperatureSensor::getCelsius() {
   _sensors->requestTemperatures();
   float tempC = _sensors->getTempCByIndex(0);
