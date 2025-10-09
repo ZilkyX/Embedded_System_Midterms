290e74f57a3ef4ef3bee52db4359aaa09e549817
Zilyan Kyle Lorenzo <112626243+ZilkyX@users.noreply.github.com>
2025-10-09 22:35:58 +0800

    Update config.h

--- midterm/config.h
+++ midterm/config.h
@@ -1,13 +1,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+//Temperature sensor pin
 #define DS18B20_PIN 4
 
+//TDS sensor pin
 #define TDS_PIN A0
 
+//PH level sensor pin and calibration
 #define PH_PIN A1
 #define PH_CALIBRATION 22.34
 
+//OLED Display Configs
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1  
