c92c315207338499cb34f034f493e56f91ada758
Imong-Jayy <andrei0436iz@gmail.com>
2025-10-09 22:32:22 +0800

    Update Tds_Sensor.h

--- midterm/Tds_Sensor.h
+++ midterm/Tds_Sensor.h
@@ -1,14 +1,17 @@
+//Prevents reddefinition if not defined
 #ifndef TDS_H
+//define
 #define TDS_H
 
 #include <Arduino.h>
 
 class TDSSensor {
+  //Member Function Interface
   public:
     TDSSensor(byte pin, float vref = 5.0, int scount = 30);
     void begin();
     float getTDS(float temperature = 25.0);
-
+  // Member Variable Interface
   private:
     int getMedianNum(int bArray[], int filterLen);
     byte _pin;
